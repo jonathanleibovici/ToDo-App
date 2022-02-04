cf8e1e3bdbe576d66ec27dfd2c9d116148e52be9
Jonathan Leibovici <jonathanleibovici@jonathans-air.myfiosgateway.com>
2022-02-03 19:29:51 -0500

    cap the D in Delete

--- client/src/components/Listanime.js
+++ client/src/components/Listanime.js
@@ -65,7 +65,7 @@ const Listanime = () => {
 									className="btn btn-danger"
 									onClick={() => deleteAnime(anime.anime_id)}
 								>
-									delete
+									Delete
 								</button>
 							</td>
 						</tr>
